db7d6a23a12ff634adb0216ca4679f8e6a0f7b9a
Max Dawkins <max.dawkins@gmail.com>
2026-07-09 21:00:09 +0000

    Generalize Gluon DSA attention support

--- tokenspeed-kernel/test/test_kernel_api_selection.py
+++ tokenspeed-kernel/test/test_kernel_api_selection.py
@@ -1093,7 +1093,7 @@ _CASES = [
         "cdna4",
         "attention",
         "dsa_decode_topk",
-        "triton_dsa_decode_topk_fp8",
+        "gluon_dsa_decode_topk_fp8_gfx950",
         _attention_dsa_decode_topk,
     ),
     _case(
@@ -1101,7 +1101,7 @@ _CASES = [
         "cdna4",
         "attention",
         "dsa_prefill_topk",
-        "triton_dsa_prefill_topk_fp8",
+        "gluon_dsa_prefill_topk_fp8_gfx950",
         _attention_dsa_prefill_topk,
     ),
     _case(
